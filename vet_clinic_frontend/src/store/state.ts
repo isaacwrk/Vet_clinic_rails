eb9ddcf00b9737a5b4eac7ce23a424dec9a2af7a
isaacwrk <isaacsouzawrk@gmail.com>
2023-04-20 16:33:14 -0300

    front: creating states and mutations

--- vet_clinic_frontend/src/store/state.ts
+++ vet_clinic_frontend/src/store/state.ts
@@ -6,4 +6,33 @@ export type Prescription = {
     livestrock:string;
     completed:boolean;
     editing:boolean;
-}
+};
+
+export type Data = {
+    vet_prescription: string;
+    prescribed_by: string;
+    disease:string;
+    livestock:string;
+};
+
+export type State = {
+    loading:boolean;
+    prescriptions: Prescription[];
+    data: Data | null;
+    showCreateModal:boolean;
+    showEditModal:boolean;
+    showPrescriptionModal:boolean;
+    editModalPrescriptionId:number | undefined;
+    showPrescriptionId: number | undefined;
+};
+
+export const state: State = {
+    loading:false,
+    prescriptions:[],
+    data:null,
+    showCreateModal:false,
+    showEditModal:false,
+    showPrescriptionModal:false,
+    editModalPrescriptionId: undefined,
+    showPrescriptionId: undefined
+};
