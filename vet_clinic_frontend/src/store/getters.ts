fb0d8fc08ebdd40d9bceef402e74970c45afc1f2
isaacwrk <isaacsouzawrk@gmail.com>
2023-04-20 17:03:52 -0300

    creating getters and actions

---
+++ vet_clinic_frontend/src/store/getters.ts
@@ -0,0 +1,19 @@
+import { GetterTree } from "vuex";
+import { State, Prescription } from "./state";
+
+export type Getters = {
+    completedPrescriptionCount(state: State): number;
+    totalPrescriptionCount(state: State): number;
+    getPrescriptionById(state: State): (id: number) => Prescription | undefined;
+  };
+  export const getters: GetterTree<State, State> & Getters = {
+    completedPrescriptionCount(state) {
+      return state.prescriptions.filter((prescription) => prescription.completed).length;
+    },
+    totalPrescriptionCount(state) {
+      return state.prescriptions.length;
+    },
+    getPrescriptionById: (state) => (id: number) => {
+      return state.prescriptions.find((prescription) => prescription.id === id);
+    },
+  };
